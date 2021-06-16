b073e27e26885e2cb03791530efd8e81b93df6ec
Alandragonz <alan_cravioto@hotmail.com>
2021-06-16 15:41:31 -0500

    cript item

---
+++ Assets/Scripts/ConstructorItem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstructorItem : MonoBehaviour
+{
+    public string ItemId;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
